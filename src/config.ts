de382cc6a139f66ba92826841ee689f8c0add9d8
Yehorii <159281281+Rotelanin@users.noreply.github.com>
2026-06-13 17:03:33 +0000

    attempt #2 with \b

--- src/config.ts
+++ src/config.ts
@@ -39,7 +39,7 @@ export const bannedWords: {
 		//>:( -dart
 		// "uwu", //lol
 
-		"nig"+"ger", "nig"+"ga", "niger", "ni8"+"8er", "nig"+"gre", "негр", "ниг"+"гер", "нигер", "нігер", "ніг"+"гер", "negr", //our apologies to citizens of the Republic of Niger
+		"nig"+"ger", "nig"+"ga", "niger", "ni8"+"8er", "nig"+"gre", "негр", "ниг"+"гер", "нигер", "нігер", "ніг"+"гер", /\bnegr\b/, //our apologies to citizens of the Republic of Niger
 		["ni"+"ga", "anniga", "inniga", "unniga", "aniga", "iniga", "eniga", "oniga"],
 		"re"+"tard",
 		'kill yourself', 'kill urself', /\bkys\b/,
@@ -70,7 +70,7 @@ export const bannedWords: {
 	]),
 	/** autoWhack: new players saying one of these words will be automatically stopped and muted. */
 	autoWhack: [
-		"nig"+"ger","nig"+"ga","ni8"+"8er","nig"+"g3r","hit"+"ler","fa"+"gg"+"ot","nazis", "негр", "ниг"+"гер", "нигер", "нігер", "ніг"+"гер", "negr"
+		"nig"+"ger","nig"+"ga","ni8"+"8er","nig"+"g3r","hit"+"ler","fa"+"gg"+"ot","nazis", "негр", "ниг"+"гер", "нигер", "нігер", "ніг"+"гер", /\bnegr\b/
 	],
 };
 
